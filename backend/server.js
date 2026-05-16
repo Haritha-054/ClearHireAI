3b865e6695f29a590fc1708e10abccbe356a5931
Dondeti Haritha Reddy <harithareddydondeti5454@gmail.com>
2026-05-17 00:14:33 +0530

    fixed evaluator service-3

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const axios = require('axios')
 const uploadRouter = require('./routes/upload')
 
 const app = express()
